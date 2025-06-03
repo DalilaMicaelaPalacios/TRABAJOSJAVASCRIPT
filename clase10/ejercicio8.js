b828c7603ca744ac7b6779e5e297e04e6894b08d
Dalila <dalimicapalaa91@gmail.com>
2025-06-02 22:59:41 -0300

    de celsius a fahrenheit

--- clase10/ejercicio8.js
+++ clase10/ejercicio8.js
@@ -11,10 +11,8 @@ const prompt = require('prompt-sync')();
 // Paso 1: Se pide al usuario que ingrese la temperatura en grados Celsius
 let celsius = parseFloat(prompt("Ingrese la temperatura en grados Celsius: "));
 
-
 // Paso 2: con la fórmula investigada se pasa de grados celsius a grados Fahrenheit 
 let fahrenheit = celsius * 9 / 5 + 32;
-
 
 // Paso 3: se muestra el resultado en la consola
 console.log(`La temperatura en grados Fahrenheit es: ${fahrenheit}`);
